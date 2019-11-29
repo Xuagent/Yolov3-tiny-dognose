02dc3424f961f2ad46d0874263d10e192ca91353
Xuagent <xuqiang201012@163.com>
2019-11-29 13:52:14 +0800

    frist commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+# Author: Agent Xu
+from setuptools import setup, find_packages
+
+setup(
+	name = "yolov3_tiny_dognose",
+	version = "0.1",
+	url = 'https://github.com/Xuagent/Yolov3-tiny-dognose.git',
+	long_description = open('README.md').read(),
+	packages = find_packages(),
+)
